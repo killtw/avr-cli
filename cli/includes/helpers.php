ed3884694e7326946b2ee2e650aaa8a6b4776e60
Karl Li <killtw@gmail.com>
2018-01-07 17:26:38 +0800

    add warning

--- cli/includes/helpers.php
+++ cli/includes/helpers.php
@@ -3,8 +3,22 @@
 use Symfony\Component\Console\Output\ConsoleOutput;
 
 /**
- * @param string $messages
+ * @param string|array $messages
  */
 function output($messages) {
-    (new ConsoleOutput)->writeln($messages);
+    if (is_array($messages)) {
+        foreach ($messages as $message) {
+            (new ConsoleOutput)->writeln($message);
+        }
+    } else {
+        (new ConsoleOutput)->writeln($messages);
+    }
+}
+
+/**
+ * @param string $messages
+ */
+function warning($messages)
+{
+    output("<fg=red>$messages</>");
 }
